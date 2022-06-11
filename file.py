0245d6b91a4ba1a30db6690207ccc165a54f3355
Raeesh Rayeen <raeeshrayeen98@gmail.com>
2022-06-12 02:06:14 +0530

    'second_modify'

--- file.py
+++ file.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-
 app=FastAPI()
-
+c="raeesh"
 @app.get('/')
-async def file_name():
-    return {"course":"fast_api"}
+async def name1():
+    a=5
+    b=6
+    return f'This is value of a {a} and this is b {b} {c}'
